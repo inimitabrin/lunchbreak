b8b9cff24821f5ff65cd85f1e6de3b884f1a57af
inimitabrin <breanna.m.bliss@gmail.com>
2017-04-06 21:47:15 -0700

    Add return to location button

--- js/map.js
+++ js/map.js
@@ -1,47 +1,77 @@
 $(document).ready(function() {
 
-var allLatlng = [];
-var allMarkers = [];
-var infowindow = null;
-var pos;
-var userCords;
-var tempMarkerHolder = [];
+function createMap (lat, lng, zoomVal) {
+        var mapOptions = { 
+          center: new google.maps.LatLng(lat,lng),
+          zoom: zoomVal,
+          scrollwheel: false,
+          gestureHandling: 'cooperative',
+          zoomControl: true,
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_CENTER
+          },
+          scaleControl: false,
+          streetViewControl: false,
+          mapTypeControl: false
+        };
+      map = new google.maps.Map(document.getElementById("map"), mapOptions); 
+      }  
+      function initMap() { 
+      if(navigator.geolocation) {  
+      success = function(position) {    
+      createMap(position.coords.latitude, position.coords.longitude,13); 
+       var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            createMap(position.coords.latitude, position.coords.longitude, 15);
 
-if (navigator.geolocation) {
-  function error(err) {
-    console.warn('ERROR (' + err.code + '): ' + err.message);
-  }
+            var marker = new google.maps.Marker({
+              position: pos,
+              title: "Present Location"
+            });
 
-  function success(pos) {
-    userCords = pos.coords;
-  }
+            map.setCenter(pos);
+            marker.setMap(map); 
+           var customControlDiv = document.createElement('div');
+var customControl = new CustomControl(customControlDiv, map);
 
-  navigator.geolocation.getCurrentPosition(success, error);
-  //console.log(pos.latitude + " " + pos.longitude);
-} else {
-    alert ('Geolocation is not supported in your browser');
-}
+customControlDiv.index = 1;
+map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(customControlDiv);
 
-var mapOptions = {
-  zoom: 5,
-  center: new google.maps.LatLng(37.09024, -100.712891),
-  panControl: true,
-  panControlOptions: {
-    position: google.maps.ControlPosition.BOTTOM_LEFT
-  },
-  zoomControl: true,
-  zoomControlOptions: {
-    style: google.maps.ZoomControlStyle.LARGE,
-    position: google.maps.ControlPosition.RIGHT_CENTER
-  },
-  scaleControl: false
-};
+function CustomControl(controlDiv, map) {
 
-infowindow = new google.maps.InfoWindow({
-  content: "holding..."
-});
+    // Set CSS for the control border
+    var controlUI = document.createElement('div');
+    controlUI.style.marginTop = '5px';
+    controlUI.style.marginLeft = '-6px';
+    controlUI.style.paddingTop = '1px';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to set the map to Home';
+    controlDiv.appendChild(controlUI);
 
+    // Set CSS for the control interior
+    var controlText = document.createElement('div');
+    controlText.style.fontFamily = 'Arial,sans-serif';
+    controlText.style.fontSize = '10px';
+    controlText.style.paddingLeft = '4px';
+    controlText.style.paddingRight = '4px';
+    controlText.style.marginTop = '-8px';
+    controlText.innerHTML = '<img src="images/currentloc.png">';
+    controlUI.appendChild(controlText);
 
-var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+    // Setup the click event listeners
+    google.maps.event.addDomListener(controlUI, 'click', function () {
+        map.setCenter(pos);
+    });
+    } 
+        };   
+      error = function() {
+      createMap(99.648493, -99.410812,12); 
+      }    
+      navigator.geolocation.getCurrentPosition(success, error);  
+        } 
+    }
+initMap();
 });
